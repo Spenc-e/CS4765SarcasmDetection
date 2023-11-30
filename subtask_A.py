3b90aa3f5370c0bb2cd1a3c2610273a2ec0553cd
Corey Mullin <113045941+CoreyMullin@users.noreply.github.com>
2023-11-30 19:40:53 -0400

    Made changes to subtask_A.py

--- subtask_A.py
+++ subtask_A.py
@@ -4,21 +4,16 @@ import csv
 from collections import defaultdict
 import sys
 import re
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
-def tokenize(text, version=''):
-    tokens =[]
-    if version == 'SE':
-        tokens = ['<'+token+'>' for token in re.findall(r'[@#]?\b\w+\b[!?]?', text.casefold(), flags=re.UNICODE)] #gives us start and end info
-    else:
-        tokens = re.findall(r'[@#]?\b\w+\b[!?]?', text.casefold(), flags=re.UNICODE) #keeps words, @ and # at the start of word, and ?! at end
-    print(tokens)
-    return tokens
+# The regular expression looks for words that possibly start with '@' 
+# or '#' and possibly end with '!' or '?'. The '@' or '#' and the '!' 
+# or '?' are optional. It captures words, mentions, or hashtags in a text.
+# this information may prove useful when analyzing tweets.
+custom_token_pattern = r'(?u)[@#]?\b\w+\b[!?]?'
 
 # A most-frequent class baseline
 class Baseline:
@@ -41,7 +36,7 @@ if __name__ == '__main__':
     import sys
     sys.stdout.reconfigure(encoding='utf-8')
 
-    # Method will be one of 'baseline', 'lr', 'nb', or 'nbse'
+    # Method will be one of 'baseline', 'lr', 'nb'
     method = sys.argv[1]
     train_data = sys.argv[2]
     test_data = sys.argv[3]
@@ -75,28 +70,24 @@ if __name__ == '__main__':
     # Splitting the train data into dev data. Specifying random state to allow reproducibility
     train_docs, dev_docs,train_labels,dev_labels = train_test_split(train_text, train_klasses, test_size=0.2, random_state=4)
 
-
     if method == "nb":
+
+        # Allows you to set the ngram size. (3,3) is trigram
+        # (1,3) is unigram to trigram. If you want to individually test
+        # different ngram sizes, you must set the range to be the same
+        # number on either side of the comma. e.g. (2,2) = bigram 
+        ngram_custom_range = (3,3)
         
-        ngram_size = 3
+        # tokenizing text, using a custom regular expression defined above using ngram classification.
+        count_vectorizer = CountVectorizer(token_pattern=custom_token_pattern,ngram_range=ngram_custom_range)
 
-        # count_vectorizer = CountVectorizer(analyzer=tokenize,ngram_range=(1,ngram_size))
-        count_vectorizer = CountVectorizer(ngram_range=(2,4)) #ngram_range=(2,4) seems to be best
-
-
+        #Each cell in the matrix indicates the frequency of a type in the documents.
         train_counts = count_vectorizer.fit_transform(train_text)
         test_counts = count_vectorizer.transform(test_text)
 
 
-
-        # NB_model = make_pipeline(CountVectorizer(ngram_range=(1,ngram_size)), MultinomialNB())
-
         NB_model = MultinomialNB()
-
         NB_model.fit(train_counts, train_klasses)
-
-
-
         results = NB_model.predict(test_counts)
 
     elif method == "baseline":
@@ -107,52 +98,27 @@ if __name__ == '__main__':
         from sklearn.feature_extraction.text import CountVectorizer
         from sklearn.linear_model import LogisticRegression
 
-        # sklearn provides functionality for tokenizing text and
-        # extracting features from it. This uses the tokenize function
-        # defined above for tokenization (as opposed to sklearn's
-        # default tokenization) so the results can be more easily
-        # compared with those using NB.
-        # http://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
-        count_vectorizer = CountVectorizer(analyzer=tokenize)
+        # tokenizing text, using a custom regular expression defined above
+        count_vectorizer = CountVectorizer(token_pattern=custom_token_pattern)
 
-        # train_counts will be a DxV matrix where D is the number of
-        # training documents and V is the number of types in the
-        # training documents. Each cell in the matrix indicates the
-        # frequency (count) of a type in a document.
-        train_counts = count_vectorizer.fit_transform(train_docs)
-
-        print(train_counts.shape)
-        print(len(train_labels))
+        #Each cell in the matrix indicates the frequency of a type in the documents.
+        train_counts = count_vectorizer.fit_transform(train_text)
+        test_counts = count_vectorizer.transform(test_text)
 
 
-        # Train a logistic regression classifier on the training
-        # data. A wide range of options are available. This does
-        # something similar to what we saw in class, i.e., multinomial
-        # logistic regression (multi_class='multinomial') using
-        # stochastic average gradient descent (solver='sag') with L2
-        # regularization (penalty='l2'). The maximum number of
-        # iterations is set to 1000 (max_iter=1000) to allow the model
-        # to converge. The random_state is set to 0 (an arbitrarily
-        # chosen number) to help ensure results are consistent from
-        # run to run.
-        # http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
+        # Training a logistic regression classifier on the train data.
+        # The random state is set to an arbitrary number to allow
+        # reproducibility in the results
+
         lr = LogisticRegression(multi_class='multinomial',
                                 solver='sag',
                                 penalty='l2',
                                 max_iter=1000,
                                 random_state=0)
-        clf = lr.fit(train_counts, train_labels)
+        clf = lr.fit(train_counts, train_klasses)
 
-        # Transform the test documents into a DxV matrix, similar to
-        # that for the training documents, where D is the number of
-        # test documents, and V is the number of types in the training
-        # documents.
-        test_counts = count_vectorizer.transform(test_text)
         # Predict the class for each test document
         results = clf.predict(test_counts)
 
-
-# for x in results:
-#         print(x)
-
+# Printing the precision, recall, f1-score, macro avg, weight avg, and accuracy
 print(metrics.classification_report(test_klasses, results,zero_division=1))
